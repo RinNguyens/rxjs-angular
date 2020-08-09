33ca3c673213008035f05b3d361c62ad69f4353c
rinnguyen <rinchans@rinnguyens-MacBook-Pro.local>
2020-08-09 15:07:28 +0700

    update code your branch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,71 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { from, Observable, Subscription } from 'rxjs';
+import { map, filter, first, find } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'rxjs-observable';
+export class AppComponent implements OnInit, OnDestroy {
+
+  sub = new Subscription();
+
+  ngOnInit(): void {
+    const observable = new Observable((item) => {
+      console.log('just have data');
+      item.next(1);
+      item.next(2);
+      item.next(3);
+      console.log('end data');
+    });
+
+    observable.subscribe((x) => {
+      console.log('got value', x);
+    });
+
+    console.log('------------Even number--------------');
+
+    from([1, 2, 3, 4, 5, 6, 7, 8])
+      .pipe(
+        filter((x) => x % 2 === 0) // so chan
+      )
+      .subscribe((x) => {
+        console.log(x); // 2,4,6,8
+      });
+
+    // pipe with first() : it will get two propety optional: predicate and defaultvalue
+    // if condition -> true -> get value first. else false -> show EmptyError
+    console.log('------------First(x > 3)---------------')
+    from([1, 2, 3, 4, 5, 6, 7, 8])
+      .pipe(
+        first((x) => x > 3) // get value first x > 3
+      )
+      .subscribe((x) => {
+        console.log(x);
+      });
+
+    // last() : even get 2 propety: predicate and defaultvalue
+    // it witll get value last -> complete or show Error when not value
+
+
+    // find :the same array.prototype.find() of js, find() emmit value first
+    console.log('------------------Find(% 5---------------)');
+    from([1, 2, 3, 4, 5, 6, 7])
+      .pipe(
+        find((x) => x % 5 === 0) // % 5
+      )
+      .subscribe((x) => {
+        console.log(x);
+      })
+
+    // single() -> just emmit first value. If > 1 value -> false -> show Error
+
+
+  }
+
+  ngOnDestroy(): void {
+    this.sub.unsubscribe();
+  }
+
 }
